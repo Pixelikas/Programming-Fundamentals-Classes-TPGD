e2e2326718e5b3ac57a9a2ac1525e80819881107
Lucas Naspolini <67108278+Pixelikas@users.noreply.github.com>
2026-08-19 10:36:29 -0300

    Add files via upload

---
+++ POO - Abstração/Assets/Scripts/Inimigo.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public abstract class Inimigo : MonoBehaviour
+{   
+    public abstract void Pular();
+    // public abstract void Atacar();
+    // public abstract void Mover();
+
+}
